8340475420cdb09a17d90e09db1800f7b43e0852
fayjul <fayjul79@gmail.com>
2023-08-22 07:20:02 +0600

    Asyncronous added

--- Backend/API/Controllers/UserController.cs
+++ Backend/API/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -6,20 +7,15 @@ namespace API.Controllers;
 [Route("api/[controller]")]
 public class UserController : ControllerBase
 {
-    private readonly DataContext _context ;
+    private readonly DataContext _context;
     public UserController(DataContext context)
     {
         _context = context;
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
-    {
-        var users = _context.Users.ToList();
-
-        return users;
-    }
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() => await _context.Users.ToListAsync();
 
     [HttpGet("{id}")]
-    public ActionResult<AppUser> GetUser(int id) => _context.Users.Find(id);
+    public async Task<ActionResult<AppUser>> GetUser(int id) => await _context.Users.FindAsync(id);
 }
