b1836ceb95f1d5227ee6d21987a84c4624d20c72
fayjul <fayjul79@gmail.com>
2023-09-25 10:02:17 +0600

    some css added

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
   styleUrls: ['./member-card.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MemberCardComponent {
   @Input() member: Member | undefined = {} as Member;
