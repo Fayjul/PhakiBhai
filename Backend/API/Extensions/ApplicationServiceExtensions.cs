f3d04b9750adf49eeecfdd262584345126539702
fayjul <fayjul79@gmail.com>
2024-01-24 08:07:22 +0600

    Last active filter added in backend

--- Backend/API/Extensions/ApplicationServiceExtensions.cs
+++ Backend/API/Extensions/ApplicationServiceExtensions.cs
@@ -11,21 +11,22 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services,
          IConfiguration configuration)
-         {
+        {
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
             });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<CloudinarySettings>(configuration.GetSection("CloudinarySettings"));
             services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<LogUserActivity>();
 
             return services;
-         }
+        }
     }
 }
